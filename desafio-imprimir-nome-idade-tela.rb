073521e897c37c976310f7b0614f952e8458397d
samueloereiradev <samuelzoly@gmail.com>
2023-07-06 19:13:55 -0300

    segundo commit

---
+++ desafio-imprimir-nome-idade-tela.rb
@@ -0,0 +1,8 @@
+# Programa que recebe o nome e a idade de um usuário e imprime na tela
+print "Por Favor, digite seu nome e sobrenome: "
+user_name = gets.chomp
+print "agora sua idade: "
+age_user = gets.chomp
+
+puts("Seu nome é #{user_name} e sua idade é #{age_user} anos!")
+
